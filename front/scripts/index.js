9d624b9590723bfd3029a9af00a72659ced25a88
denisefussi <denisefussi@gmail.com>
2024-03-05 15:09:09 -0300

    cambios css

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,2 +1,23 @@
-temData.map()
+const filmsSection = document.getElementById('films') 
+
+function films(movie) {
+    const movieElement = document.createElement('div')
+    const containerMovie = document.createElement('div')
+    containerMovie.classList.add('div-movie')
+
+    movieElement.innerHTML = `<img src="${movie.poster}" alt="${movie.title}">`
+
+    containerMovie.innerHTML = `
+    <h3>${movie.title} (${movie.year})</h3>
+    <p><strong>Director:</strong> ${movie.director}</p>
+    <p><strong>Duracion:</strong> ${movie.duration}</p>
+    <p><strong>Genero:</strong> ${movie.genre.join(', ')}</p>
+    <p><strong>Rate:</strong> ${movie.rate}</p>
+    `
+
+    filmsSection.appendChild(movieElement)
+    movieElement.appendChild(containerMovie)
+}
+tempData.forEach(films)
+
 console.log(tempData);
