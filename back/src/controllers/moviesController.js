acea516023a0a83d7e7f0fb469baef9c440d8b16
denisefussi <denisefussi@gmail.com>
2024-03-21 12:19:09 -0300

    proyecto corregido y ok

--- back/src/controllers/moviesController.js
+++ back/src/controllers/moviesController.js
@@ -1,28 +1,36 @@
-const { getFilms, postFilms } = require("../services/movies")
+const { getFilms, postFilms } = require("../services/movies");
 
 const moviesController = async (req, res) => {
-    try {
-        const response = await getFilms();
-        res.status(200).json(response)
-    } catch (error) {
-        res.status(500).send(error.message)
-    }
-}
+  try {
+    const response = await getFilms();
+    res.status(200).json({error: false, data: response});
+  } catch (error) {
+    res.status(500).send({error: true, message:error.message});
+  }
+};
 
-const postMoviesController = async (req, res)=>{
-
-    try {
-      await postFilms(req.body)
-
-      res.status(201).json({message:"Peli creada exitosamente"})
-
-    } catch (error) {
-        res.status(400).json({error:error.message})
-    }
-
-}
+const postMoviesController = async (req, res) => {
+  try {
+    const { title, year, director, duration, genre, rate, poster } = req.body
+    const postMovie = await postFilms({
+        title, 
+        year, 
+        director, 
+        duration, 
+        genre, 
+        rate, 
+        poster   
+    });
+    res.status(201).json({error: false, data:postMovie, message: "Peli creada exitosamente" });
+  } catch (error) {
+    res.status(400).json({ error: true, message: error.message });
+  }
+};
 //si hay un error en la ruta ovies, va a caer en mi catch, en mi error 500. sino, 404.
 module.exports = {
-    moviesController,
-    postMoviesController
+  moviesController,
+  postMoviesController,
 };
+
+//const postawait postFilms(req.body);
+//
--- back/src/services/movies.js
+++ back/src/services/movies.js
@@ -1,38 +1,38 @@
-const { Movies } = require("../types/class")
+const { Movies } = require("../types/class");
 //const moviesValidation = require('../middlewares/moviesVlidations')
-const Movie = require('../db/models/Movie')
+const Movie = require("../db/models/Movie");
 
 module.exports = {
-    getFilms: async () => {
-    try {//declaramos una respuesta
-        const movies = await Movie.find()
-        //const moviesMap = movies.map((movie) => {
-            // if(moviesValidation(movie)){
-            //     return new Movies(movie);
-            // }
-        //})
-        return movies;
+  getFilms: async () => {
+    try {
+      //declaramos una respuesta
+      const movies = await Movie.find();
+      //const moviesMap = movies.map((movie) => {
+      // if(moviesValidation(movie)){
+      //     return new Movies(movie);
+      // }
+      //})
+      return movies;
     } catch (error) {
-        throw new Error(error.message);        
+      throw new Error(error.message);
     }
-},
-   
-    postFilms: async (movie) => {
-        try {
-            const newMovie = await Movie.create(movie)
+  },
 
-            return newMovie 
-        } catch (error) {
-            throw new Error(error.message);  
-        } 
-    },
-}
+  postFilms: async (movie) => {
+    try {
+      const newMovie = await Movie.create(movie);
+
+      return newMovie;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  },
+};
 //     updateMovie: async (title) => {
 //         try {
 //             Movie.findOneAndUpdate({ title }, { $inc: {}})
 //         } catch (error) {
-//             throw new Error(error.message);  
+//             throw new Error(error.message);
 //         }
 //     },
 // }
-
